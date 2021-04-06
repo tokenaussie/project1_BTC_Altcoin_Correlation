00d01f4b289e8febf2e4c57bfea7e301d6e84729
James Sheridan <77235108+JSheridanAU@users.noreply.github.com>
2021-04-06 19:02:40 +0930

    Now returns as 'float64'
    Also removes ',' from prices

--- reader.py
+++ reader.py
@@ -9,5 +9,6 @@ def reader(file1, ticker1):
 
 # Use the file path to read the CSV into a DataFrame and display a few rows
     eth_df = pd.read_csv(csv_path, parse_dates=True, index_col="Date", infer_datetime_format=True).drop(columns=["Open", "High", "Low", "Vol.", "Change %"])
+    eth_df = eth_df.replace(',', '', regex = True).astype('float64')
     eth_df.columns=[ticker1]
     return eth_df[::-1]
